909febedb7edfae4697333228d81ffd6eff1a0d6
Arrest0ron <dimon8013@gmail.com>
2024-03-17 23:27:31 +0300

    perlin noise test

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,118 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Window/Keyboard.hpp>
-#include <vector>
-#include "textures.h"
+#include <iostream>
 #include <cmath>
+
+const int MAP_WIDTH = 128;
+const int MAP_HEIGHT = 128;
+const int TILE_SIZE = 16;
+
+enum TileType {
+    Air,
+    Cloud,
+    Grass,
+    Dirt,
+    Water
+};
+
+// Функция для вычисления шума Перлина в заданных координатах
+float perlin(float x, float y) {
+    int n = x + y * 57;
+    n = (n << 13) ^ n;
+    return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+}
+
+// Функция для сглаживания шума Перлина
+float smoothNoise(float x, float y) {
+    float corners = (perlin(x - 1, y - 1) + perlin(x + 1, y - 1) + perlin(x - 1, y + 1) + perlin(x + 1, y + 1)) / 16;
+    float sides = (perlin(x - 1, y) + perlin(x + 1, y) + perlin(x, y - 1) + perlin(x, y + 1)) / 8;
+    float center = perlin(x, y) / 4;
+    return corners + sides + center;
+}
+
+// Функция для интерполяции
+float interpolate(float a, float b, float x) {
+    float ft = x * 3.1415927;
+    float f = (1 - cos(ft)) * 0.5;
+    return a * (1 - f) + b * f;
+}
+
+// Функция для генерации окончательного шума Перлина
+float interpolatedNoise(float x, float y) {
+    int integer_X = int(x);
+    float fractional_X = x - integer_X;
+    int integer_Y = int(y);
+    float fractional_Y = y - integer_Y;
+    float v1 = smoothNoise(integer_X, integer_Y);
+    float v2 = smoothNoise(integer_X + 1, integer_Y);
+    float v3 = smoothNoise(integer_X, integer_Y + 1);
+    float v4 = smoothNoise(integer_X + 1, integer_Y + 1);
+    float i1 = interpolate(v1, v2, fractional_X);
+    float i2 = interpolate(v3, v4, fractional_X);
+    return interpolate(i1, i2, fractional_Y);
+}
 
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(512, 256), "Particles");
+    // Создаем окно размером 128*16 x 128*16 пикселей
+    sf::RenderWindow window(sf::VideoMode(MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE), "Tilemap");
 
-    // create the particle system
-    ParticleSystem particles(1000);
+    // Создаем двумерный массив для хранения типов тайлов
+    TileType tileMap[MAP_WIDTH][MAP_HEIGHT];
 
-    // create a clock to track the elapsed time
-    sf::Clock clock;
+    // Генерируем карту с использованием шума Перлина
+    for (int x = 0; x < MAP_WIDTH; ++x) {
+        for (int y = 0; y < MAP_HEIGHT; ++y) {
+            float noise = interpolatedNoise(x / 16.0f, y / 16.0f); // Масштабируем координаты для более гладкого шума
+            if (noise > 0.5) {
+                tileMap[x][y] = Dirt;
+            } else if (noise > 0.4) {
+                tileMap[x][y] = Grass;
+            } else if (noise > 0.3) {
+                tileMap[x][y] = Water;
+            } else if (noise > 0.2) {
+                tileMap[x][y] = Cloud;
+            } else {
+                tileMap[x][y] = Air;
+            }
+        }
+    }
 
-    // particles.setTexture("the.png");
-
-    // run the main loop
+    // Главный цикл программы
     while (window.isOpen())
     {
-        // handle events
+        // Проверка событий
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        // make the particle system emitter follow the mouse
-        sf::Vector2i mouse = sf::Mouse::getPosition(window);
-        particles.setEmitter(window.mapPixelToCoords(mouse));
-
-        // update it
-        sf::Time elapsed = clock.restart();
-        particles.update(elapsed);
-
-        // draw it
-        window.clear();
-        
-        window.draw(particles);
+        // Отрисовка тайлов
+        window.clear(sf::Color::Blue); // Фон - голубой цвет
+        for (int x = 0; x < MAP_WIDTH; ++x) {
+            for (int y = 0; y < MAP_HEIGHT; ++y) {
+                sf::RectangleShape tile(sf::Vector2f(TILE_SIZE, TILE_SIZE));
+                tile.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+                switch (tileMap[x][y]) {
+                    case Grass:
+                        tile.setFillColor(sf::Color::Green);
+                        break;
+                    case Dirt:
+                        tile.setFillColor(sf::Color(139, 69, 19)); // Коричневый цвет для земли
+                        break;
+                    case Water:
+                        tile.setFillColor(sf::Color::Blue);
+                        break;
+                    case Cloud:
+                        tile.setFillColor(sf::Color::White);
+                        break;
+                    default:
+                        continue;
+                }
+                window.draw(tile);
+            }
+        }
         window.display();
     }
 
--- src/textures.h
+++ src/textures.h
@@ -1,44 +1,48 @@
-#include <SFML/Graphics.hpp>
-#include <complex>
-// void DrawFractal(sf::RenderWindow& window,double color1,double color2,int maxIterations);
-
-class ParticleSystem : public sf::Drawable, public sf::Transformable
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+class TileMap : public sf::Drawable, public sf::Transformable
 {
 public:
 
-    ParticleSystem(unsigned int count) :
-    m_particles(count),
-    m_vertices(sf::Points, count),
-    m_lifetime(sf::seconds(4)),
-    m_emitter(0, 0)
+    bool load(const std::string& tileset, sf::Vector2u tileSize, int** tiles, unsigned int width, unsigned int height)
     {
-    }
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
 
-    void setEmitter(sf::Vector2f position)
-    {
-        m_emitter = position;
-    }
-    //test
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(width * height * 4);
 
-    void update(sf::Time elapsed)
-    {
-        for (std::size_t i = 0; i < m_particles.size(); ++i)
-        {
-            // update the particle lifetime
-            Particle& p = m_particles[i];
-            p.lifetime -= elapsed;
+        // populate the vertex array, with two quads per tile
+        for (unsigned int i = 0; i < width; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                int tileNumber = tiles[i][j];
 
-            // if the particle is dead, respawn it
-            if (p.lifetime <= sf::Time::Zero)
-                resetParticle(i);
+                // find its position in the tileset texture
+                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
 
-            // update the position of the corresponding vertex
-            m_vertices[i].position += p.velocity * elapsed.asSeconds();
+                // get a pointer to the quads' vertices of the current tile
+                sf::Vertex* quads = &m_vertices[(i + j * width) * 4];
 
-            // update the alpha (transparency) of the particle according to its lifetime
-            float ratio = p.lifetime.asSeconds() / m_lifetime.asSeconds();
-            m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
-        }
+                // define the 6 corners of the two quads
+                quads[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                quads[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                quads[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+                quads[3].position = sf::Vector2f(i * tileSize.x, (j+1) * tileSize.y);
+                
+
+                // define the 6 matching texture coordinates
+                quads[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                quads[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                quads[2].texCoords = sf::Vector2f((tu+1) * tileSize.x, (tv+1) * tileSize.y);
+                quads[3].texCoords = sf::Vector2f((tu ) * tileSize.x, (tv + 1) * tileSize.y);
+            }
+
+        return true;
     }
 
 private:
@@ -48,37 +52,13 @@ private:
         // apply the transform
         states.transform *= getTransform();
 
-        // our particles don't use a texture
-        states.texture = NULL;
+        // apply the tileset texture
+        states.texture = &m_tileset;
 
         // draw the vertex array
         target.draw(m_vertices, states);
     }
 
-
-private:
-
-    struct Particle
-    {
-        sf::Vector2f velocity;
-        sf::Time lifetime;
-    };
-
-    void resetParticle(std::size_t index)
-    {
-        // give a random velocity and lifetime to the particle
-        float angle = (std::rand() % 360) * 3.14f / 180.f;
-        float speed = (std::rand() % 50) + 50.f;
-        m_particles[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-        m_particles[index].lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
-
-        // reset the position of the corresponding vertex
-        m_vertices[index].position = m_emitter;
-    }
-
-    sf::Texture* texture;
-    std::vector<Particle> m_particles;
     sf::VertexArray m_vertices;
-    sf::Time m_lifetime;
-    sf::Vector2f m_emitter;
+    sf::Texture m_tileset;
 };
