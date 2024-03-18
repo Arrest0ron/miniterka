6aa0192e93406e5a7914ceb48bec34063dff127e
Arrest0ron <dimon8013@gmail.com>
2024-03-18 20:09:37 +0300

    осуществил процедурную генерацию поверхности

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,75 @@
 
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <ctime>
+
+const int MAP_HEIGHT = 900/4;
+const int MAP_LENGTH = 900/4;
+const int TILESET_SIZE = 32*32;
+const int TILESET_X = 32;
+const int MIN_SEALEVEL = 900/16-5;
+const int MAX_SEALEVEL = 900/16+5;
+
+
+int**& RandomWalkTopSmoothed(int**& map, int seed, int minSectionWidth)
+{
+    //Seed our random
+    srand(seed);
+    std::cout << seed;
+    
+    int randint = rand();
+
+    //Determine the start position
+    int lastHeight = MIN_SEALEVEL + randint%(MAX_SEALEVEL-MIN_SEALEVEL + 1);
+
+    randint = rand();
+
+    //Used to determine which direction to go
+    int nextMove = 0;
+    //Used to keep track of the current sections width
+    int sectionWidth = 0;
+
+    //Work through the array width
+    for (int x = 0; x < MAP_LENGTH; x++)
+    {
+        //Determine the next move
+        nextMove = randint%2;
+        randint = rand();
+
+        //Only change the height if we have used the current height more than the minimum required section width
+        if (nextMove == 0 && lastHeight > 0 && sectionWidth > minSectionWidth)
+        {
+            lastHeight--;
+            sectionWidth = 0;
+        }
+        else if (nextMove == 1 && lastHeight < MAP_HEIGHT && sectionWidth > minSectionWidth)
+        {
+            lastHeight++;
+            sectionWidth = 0;
+        }
+        //Increment the section width
+        sectionWidth++;
+
+        //Work our way from the height down to 0
+        for (int y = lastHeight; y < MAP_HEIGHT; y++)
+        {
+            map[y][x] = 1;
+        }
+    }
+    // for ( int i=0;i!=MAP_HEIGHT;i++){
+    //     for (int j=0;j!=MAP_LENGTH;j++){
+    //         std::cout << map[i][j] << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
+    //Return the modified map
+    return map;
+}
 
 int main()
 {
     // Создание окна
-    const int MAP_HEIGHT = 10;
-    const int MAP_LENGTH = 20;
+
 
     sf::RenderWindow window(sf::VideoMode(900, 900), "Tilemap");
 
@@ -19,31 +82,35 @@ int main()
     }
 
     // Создание спрайтов для каждого тайла из тайлсета
-    sf::Sprite tiles[16];
-    int tileSize = 16; // Размер каждого тайла
-    for (int i = 0; i < 16; ++i)
+    sf::Sprite tiles[TILESET_SIZE];
+
+    int tileSize = 4; // Размер каждого тайла
+
+    for (int i = 0; i < TILESET_SIZE; ++i)
     {
-        int x = (i % 4) * tileSize;
-        int y = (i / 4) * tileSize;
+        int x = (i % TILESET_X) * tileSize;
+        int y = (i / TILESET_X) * tileSize;
         tiles[i].setTexture(tileset);
         tiles[i].setTextureRect(sf::IntRect(x, y, tileSize, tileSize));
+        tiles[0].setColor(sf::Color::White);
+        tiles[1].setColor(sf::Color::Green);
     }
 
     // Тайлмап
 
-int tilemap[MAP_HEIGHT][MAP_LENGTH] = {
-  {0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1},
-  {1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2},
-  {2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0},
-  {0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1},
-  {1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2},
-  {2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0},
-  {0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1},
-  {1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2},
-  {2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0},
-  {0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1},
-};
+    int** tilemap = new int*[MAP_HEIGHT];
+    for (int i=0;i!=MAP_HEIGHT;i++){
+        tilemap[i] = new int[MAP_LENGTH];
+    }
 
+    for (int i= 0;i!=MAP_HEIGHT;i++){
+        for (int j=0;j!=MAP_LENGTH;j++){
+            tilemap[i][j] = 0;
+        }
+    }
+    srand(time(0));
+    int seed = rand();
+    tilemap = RandomWalkTopSmoothed(tilemap,seed,2);
     // Основной цикл приложения
     while (window.isOpen())
     {
@@ -68,6 +135,6 @@ int tilemap[MAP_HEIGHT][MAP_LENGTH] = {
         }
         window.display();
     }
-
+    delete tilemap;
     return 0;
 }
